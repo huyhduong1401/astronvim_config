098784bc1c411d51e9b4772af8caf7ddf5d9843a
Huy Duong <huy.duong@skymavis.com>
2023-09-12 11:30:18 +0700

    add more language packs

--- plugins/community.lua
+++ plugins/community.lua
@@ -2,13 +2,16 @@ return {
   -- Add the community repository of plugin specifications
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.typescript" },
   { import = "astrocommunity.colorscheme.catppuccin" },
   {
     "catppuccin",
     opts = {
-      flavour = "macchiato"
-    }
-  }
+      flavour = "macchiato",
+    },
+  },
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
